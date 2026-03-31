b0b2cc8c0ddadd112f2c3a9758ed127bf2c2fa8f
jyjunPepe012233 <jinyj0217@gmail.com>
2026-03-31 12:38:28 +0900

    add World Grid Service

---
+++ Assets/Scripts/Runtime/WorldGridService.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class WorldGridService : MonoBehaviour
+{
+	public static WorldGridService Instance { get; private set; }
+	
+	[SerializeField]
+	private Grid _grid;
+	public Grid Grid => _grid;
+
+	public void Awake()
+	{
+		Instance = this;
+		
+		if (!_grid)
+		{
+			Debug.LogError("그리드가 할당되지 않았음: " + gameObject.name);
+		}
+	}
+
+	public void OnDestroy()
+	{
+		if (Instance == this) Instance = null;
+	}
+}
